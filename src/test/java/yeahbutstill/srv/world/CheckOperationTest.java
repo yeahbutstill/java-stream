7ea255f39379eb4a1a1c6e0ee7f29bab0b190a1d
Dani <mayadani28@gmail.com>
2021-05-25 04:34:27 +0700

    Chech Operation

---
+++ src/test/java/yeahbutstill/srv/world/CheckOperationTest.java
@@ -0,0 +1,34 @@
+package yeahbutstill.srv.world;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class CheckOperationTest {
+
+    @Test
+    void testAnyMatch() {
+
+        boolean match = List.of(1,2,3,4,5,21,6,7,8,9,10).stream()
+                .anyMatch(number -> number > 20);
+        System.out.println(match);
+
+    }
+
+    @Test
+    void testAllMatch() {
+
+        boolean match = List.of(1,2,3,4,5,21,6,7,8,9,10).stream()
+                .allMatch(number -> number > 20);
+        System.out.println(match);
+    }
+
+    @Test
+    void testNoneMatch() {
+
+        boolean match = List.of(1,2,3,4,5,21,6,7,8,9,10).stream()
+                .noneMatch(number -> number > 0);
+        System.out.println(match);
+
+    }
+}
