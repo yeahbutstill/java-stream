bff712123f74b250478d7f9980a255127e388556
Dani <mayadani28@gmail.com>
2021-05-25 02:27:24 +0700

    Java Stream

--- src/test/java/yeahbutstill/srv/world/OrderingOperationTest.java
+++ src/test/java/yeahbutstill/srv/world/OrderingOperationTest.java
@@ -1,2 +1,29 @@
-package yeahbutstill.srv.world;public class OrderingOperationTest {
+package yeahbutstill.srv.world;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class OrderingOperationTest {
+
+    @Test
+    void testSorted() {
+
+        List.of("Semangka", "Apple", "Jeruk", "Melon", "Mangga", "Pisang").stream()
+                .sorted()
+                .forEach(System.out::println);
+
+    }
+
+    @Test
+    void testSortedWithComparator() {
+
+        Comparator<String> reverseComparator = Comparator.reverseOrder();
+
+        List.of("Samangka", "Apple", "Jeruk", "Melon", "Mangga", "Pisang").stream()
+                .sorted(reverseComparator)
+                .forEach(System.out::println);
+
+    }
 }
