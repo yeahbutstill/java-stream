604adebd36888eb9c03be1f63fa7222ea99b66a1
Dani <mayadani28@gmail.com>
2021-05-25 00:25:35 +0700

    filtering and distinct

--- src/test/java/yeahbutstill/srv/world/FilteringOperationTest.java
+++ src/test/java/yeahbutstill/srv/world/FilteringOperationTest.java
@@ -1,2 +1,30 @@
-package yeahbutstill.srv.world;public class FilteringOperationTest {
+package yeahbutstill.srv.world;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class FilteringOperationTest {
+
+    @Test
+    void testFilter() {
+
+        List.of("Dani", "Setiawan", "Azhilla", "Asia", "Jepang", "Cina").stream()
+                .filter(name -> name.length() > 4)
+                .forEach(System.out::println);
+
+        List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).stream()
+                .filter(number -> number % 2 == 0)
+                .forEach(System.out::println);
+
+    }
+
+    @Test
+    void testDistinct() {
+
+        List.of("Dani", "Dani", "Setiawan", "Setiawan", "Azhilla", "Asia", "Jepang", "Cina").stream()
+                .distinct()
+                .forEach(System.out::println);
+
+    }
 }
