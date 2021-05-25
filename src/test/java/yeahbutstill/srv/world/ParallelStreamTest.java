3f3851b990805971e35347e36c6f99f48af0568d
Dani <mayadani28@gmail.com>
2021-05-26 01:35:42 +0700

    Parallel Stream

--- src/test/java/yeahbutstill/srv/world/ParallelStreamTest.java
+++ src/test/java/yeahbutstill/srv/world/ParallelStreamTest.java
@@ -1,2 +1,32 @@
-package yeahbutstill.srv.world;public class ParallelStreamTest {
+package yeahbutstill.srv.world;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class ParallelStreamTest {
+
+    @Test
+    void testSequential() {
+
+        Stream.of(1,2,3,4,5,6,7,8,9,10)
+                .forEach(number -> {
+                    System.out.println(Thread.currentThread().getName() + " : " + number);
+                });
+
+    }
+
+    @Test
+    void testParallelStream() {
+
+        List<Integer> numbers = List.of(1,2,3,4,5,6,7,8,9,10);
+
+        Stream<Integer> parallelStream = numbers.stream().parallel();
+
+        parallelStream.forEach(number -> {
+            System.out.println("Thread " + Thread.currentThread() + " : " + number);
+        });
+
+    }
 }
